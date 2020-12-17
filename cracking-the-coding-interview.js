3e364cc67bdce07bc029f27cedb9d7748734349f
George Ashton <georgeashton95@gmail.com>
2020-12-17 10:07:47 +0000

    Create cracking-the-coding-interview.js

---
+++ cracking-the-coding-interview.js
@@ -0,0 +1,44 @@
+## Resume
+For each role, try to discuss your accomplishments with the following approach: "Accomplished X by implementing Y which led to z". Here's an example: 
+•  Reduced object rendering time by 75% by implementing distributed caching, leading to a 10% reduction in log-in time.
+•  Increased average match accuracy from 1.2 to 1.5 by implementing a new comparison algorithm basedon windiff.
+
+Not everything you did will fit into this approach, but the principle is the same: show what you did, how you did it, and what the results were. 
+Ideally, you should try to make the results "measurable" somehow.
+
+## Questions to ask
+ These questions will give you a good feel for what the day-to-day life is like at the company. 
+ 1. "What is the ratio of testers to developers to program managers? What is the interaction like? How does project planning happen on the team?"
+ 2. "What brought you to this company? What has been most challenging for you?"
+ 
+ These questions demonstrate your knowledge or understanding of technology. 
+ 1. "I noticed that you use technology X. How do you handle problem Y?"
+ 2. "Why did the product choose to use the X protocol over the Y protocol? I know it has benefits like A, B,C, but many companies choose not to use it because of issue o:'
+
+These questions are designed to demonstrate your passion for technology. They show that you're interested in learning and will be a strong contributor to the company. 
+1.  'I'm very interested in scalability, and I'd love to learn more about it. What opportunities are there at this company to learn about this?"
+2. "I'm not familiar with technology X, but it sounds like a very interesting solution. Could you tell me a bit more about how it works?
+
+## Interview
+### Limit Details
+Stay light on details and just state the key points. When possible, try to translate it or at least explain the impact. You can always offer the interviewer the opportunity to drill in further.
+"By examining the most common user behavior and applying the Rabin-Karp algorithm, I designed a new algorithm to reduce search from O(n) to 0( log n) in 90% of cases. I can go into more details if you'd like:' 
+
+## Focus on You, not the Team
+Pay attention to your answers. Listen for how much you say "we" versus 'T' Assume that every question is about your role, and speak to that.
+
+## Structured Answer
+Nugget First means starting your response with a "nugget" that succinctly describes what your response will be about. 
+Interviewer: "Tell me about a time you had to persuade a group of people to make a big change:' 
+Candidate:"Sure, let me tell you about the time when I convinced my school to let undergraduates teach their own courses. Initially, my school had a rule where .. :'
+
+S.A.R. (Situation, Action, Result) The S.A.R. approach means that you start off outlining the situation, then explaining the actions you took, and lastly, describing the result.
+
+## Tell me About Yourself
+A typical structure that works well for many people is essentially chronological, with the opening sentence describing their current job and the conclusion discussing their relevant and interesting hobbies outside of work. 
+1.Current Role [Headline Only]: "I'm a software engineer at  Microworks, where I've been leading theAndroid team for the last five years:'
+2.College: My background is in computer science. I did my undergrad at  Berkeley and spent a few summers working at startups, including one where I attempted to launch my own business.
+3.Post College & Onwards: After college, I wanted to get some exposure to larger corporations so I joined Amazon as a developer. It was a great experience. I learned a ton about large system design and I got toreally drive the launch of a key part of AWS. That actually showed me that I really wanted to be in a more entrepreneurial environment. 
+4. Current Role [Details]: One of my old managers from Amazon recruited me out to join her startup, which was what brought me to Microworks. Here, I did the initial system architecture, which has scaledpretty well with our rapid growth. I then took an opportunity to lead the Android team. I do manage ateam of three, but my role is primarily with technical leadership: architecture, coding, etc.
+5.Outside of Work: Outside of work, I've been participating in some hackathons-mostly doing iOSdevelopment there as a way to learn it more deeply. I'm also active as a moderator on online forumsaround Android development.
+6.Wrap Up: I'm looking now for something new, and your company caught my eye. I've always loved theconnection with the user, and I really want to get back to a smaller environment too.
